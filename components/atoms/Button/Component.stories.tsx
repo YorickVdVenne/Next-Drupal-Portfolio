50a4758b8d89d0b9fb526f70844435f16db9518c
Yorick Van de Venne <yorick.vd.venne@hotmail.nl>
2023-03-22 22:13:54 +0100

    fix: finalize linter

--- components/atoms/Button/Component.stories.tsx
+++ components/atoms/Button/Component.stories.tsx
@@ -5,7 +5,7 @@ export default {
   title: 'Atoms/Buttons'
 }
 
-export const Default = () => {
+export const Default = (): JSX.Element => {
   return (
     <div style={{ marginBlock: 'var(--spacing-4x)' }}>
       <Button>Button</Button>
