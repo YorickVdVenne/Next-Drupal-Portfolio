d0d83f216b8c65d4423d867dab0c88fe9bf6e78b
Yorick Van de Venne <yorick.vd.venne@hotmail.nl>
2023-03-25 09:03:42 +0100

    fix: storybook usage

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,71 +1,15 @@
-const path = require("path");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
-  staticDirs: ["../public"],
-  stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    {
-      name: "storybook-addon-next",
-      options: {
-        nextConfigPath: path.resolve(__dirname, "../next.config.js"),
-      },
-    },
+  "stories": [
+    "../**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/manager-webpack5",
-    "storybook-addon-next-router",
+    "@storybook/addon-interactions"
   ],
-  webpackFinal: async (config) => {
-    const fileLoaderRule = config.module.rules.find(
-      (rule) => rule.test && rule.test.test(".svg")
-    );
-    fileLoaderRule.exclude = /\.svg$/;
-    return ({
-      ...config,
-      resolve: {
-        ...config.resolve,
-        plugins: [
-          new TsconfigPathsPlugin()
-        ],
-        alias: {
-          ...config.resolve.alias,
-          'next-i18next': 'react-i18next',
-          "@api/": path.resolve(__dirname, "../api/*"),
-          "@components/": path.resolve(__dirname, "../components/*"),
-          "@white-label/*": path.resolve(__dirname, "../themes/whitelabel/*"),
-          "@generated/": path.resolve(__dirname, "../generated/*"),
-          "@pages/*": path.resolve(__dirname, "../pages/*"),
-          "@lib/*": path.resolve(__dirname, "../lib/*"),
-          "@misc/*": path.resolve(__dirname, "../misc/*"),
-          "@themes/*": path.resolve(__dirname, "../themes/*"),
-        },
-      },
-      module: {
-        ...config.module,
-        rules: [
-          ...config.module.rules,
-          {
-            test: /\.svg$/,
-            enforce: "pre",
-            use: [
-              {
-                loader: require.resolve('@svgr/webpack'),
-                options: {
-                  svgoConfig: {
-                    plugins: {
-                      removeViewBox: false
-                    }
-                  }
-                }
-              }
-            ]
-          }
-        ]
-      }
-    })
-  }
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  typescript: {reactDocgen: false }
 }
--- components/atoms/Button/Component.stories.tsx
+++ components/atoms/Button/Component.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Button } from './Component'
+import { ComponentMeta } from '@storybook/react';
 
 export default {
-  title: 'Atoms/Buttons'
-}
+  title: 'Atoms/Buttons',
+  component: Button,
+} as ComponentMeta<typeof Button>
 
 export const Default = (): JSX.Element => {
   return (
