c019412295487061288ee18f0d340032831beb60
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2023-09-18 08:50:12 +0200

    finishing touches header component

--- frontend/src/components/organisms/Header/Component.tsx
+++ frontend/src/components/organisms/Header/Component.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './styles.module.css'
 import { Button } from '@components/atoms/Button/Component'
+import gridStyles from '@components/atoms/Grid/styles.module.css'
+import clsx from 'clsx'
 
 export default function Header (): JSX.Element {
 
@@ -9,15 +11,13 @@ export default function Header (): JSX.Element {
       <h1 className={styles.h1}>Hi, my name is</h1>
       <h2 className={styles.h2}>Yorick Van de Venne.</h2>
       <h3>I build things for the web.</h3>
-      <p>
-        Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more.
-        Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more.
-      </p>
-      <p>
-        Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more.
-        Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more. <Button as='link' onClick={() => console.log('test')}>Link</Button>
-      </p>
-      <Button onClick={() => console.log('test')} as='button'>Resume</Button>
+      <div className={clsx(gridStyles.grid, styles.wrapper)}>
+        <p className={styles.text}>
+          Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more.
+          Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more. <Button as='link' onClick={() => console.log('test')}>Link</Button>
+        </p>
+        <Button onClick={() => console.log('test')} as='button' className={styles.button}>Resume</Button>
+      </div>
     </header>
   )
 }
