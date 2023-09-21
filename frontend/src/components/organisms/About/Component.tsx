4a7a795c332e36347d4eaf61f4a23aa16ea3b832
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2023-09-21 18:30:45 +0200

    feat: adds profile image to about

--- frontend/src/components/organisms/About/Component.tsx
+++ frontend/src/components/organisms/About/Component.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './styles.module.css'
 import gridStyles from '@components/atoms/Grid/styles.module.css'
 import Section from '@components/atoms/Section/Component'
+import Image from 'next/image'
+import profileImage from '../../../../public/images/profile-image.png'
 
 export default function About (): JSX.Element {
 
@@ -10,9 +12,9 @@ export default function About (): JSX.Element {
       <h2 className={styles.numberedHeading}>About me</h2>
       <div className={gridStyles.grid}>
         <div className={styles.content}>
-          <p>Content</p>
-          <p>Content</p>
-          <p>Content</p>
+          <p>Hello! My name is Yorick and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!</p>
+          <p>Hello! My name is Yorick and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!</p>
+          <p>I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.</p>
           <p>Here are a few technologies I've been working with recently:</p>
           <ul className={styles.techList}>
             <li>TypeScript</li>
@@ -24,7 +26,15 @@ export default function About (): JSX.Element {
           </ul>
         </div>
         <div className={styles.profileImage}>
-          Image
+          <div className={styles.imageWrapper}>
+            <Image 
+              src={profileImage}
+              alt='Picture of Yorick'
+              className={styles.image}
+              width={500}
+              height={500}
+            />
+          </div>
         </div>  
       </div>
     </Section>
