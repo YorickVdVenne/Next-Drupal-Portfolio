f7a2c6ba6fa688e3b4ec569d3a6b82028f34a078
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2023-10-06 21:48:00 +0200

    feat: adds nav animations

--- frontend/src/components/organisms/Navigation/Component.tsx
+++ frontend/src/components/organisms/Navigation/Component.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.module.css'
 import { Button } from '@components/atoms/Button/Component'
-import menu from '../../../../content/menu.json'
+import menu from '@content/menu.json'
 import { Logo } from '@components/atoms/Logo/Component'
+import clsx from 'clsx'
 
 export default function Navigation (): JSX.Element {
+  const [scrolled, setScrolled] = useState(false);
+  const [scrollTop, setScrollTop] = useState(true)
 
-  console.log(menu.menu.actionButton)
+  useEffect(() => {
+    let lastScrollY= 0
+
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+      
+      if (currentScrollY !== 0) {
+        setScrollTop(false) 
+      } else setScrollTop(true)
+
+      if (window.scrollY > lastScrollY) {
+          setScrolled(true)
+      } else setScrolled(false)
+
+      lastScrollY = currentScrollY
+    };
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll)
+    }
+}, [])
 
   return (
-    <nav className={styles.nav}>
+    <nav id="navbar" className={clsx(styles.nav, {
+      [styles.hidden] : scrolled,
+      [styles.visible]: !scrolled,
+      [styles.small]: !scrolled && !scrollTop
+    })}
+    >
       <div className={styles.innerNav}>
         <a href='/' className={styles.logo}>
           <Logo />
