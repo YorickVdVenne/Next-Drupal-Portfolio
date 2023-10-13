cf8fca34d0fb24be018c1b70b839881bdf289c20
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2023-10-13 11:45:17 +0200

    feat: adds socials to footer

--- frontend/src/components/organisms/Footer/Component.tsx
+++ frontend/src/components/organisms/Footer/Component.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import styles from './styles.module.css'
 import { Button } from '@components/atoms/Button/Component'
 import * as Icons from '@components/atoms/Icons/Component'
+import { IconMapper } from '@components/atoms/Icons/Component'
 
 export default function Footer (): JSX.Element {
 
   return (
     <footer className={styles.footer}>
+      <div className={styles.socials}>
+        <ul>
+          <li><a href='https://github.com/YorickVdVenne'>{IconMapper('github')}</a></li>
+          <li><a href='https://www.linkedin.com/in/yorick-van-de-venne-514121186'>{IconMapper('linkedin')}</a></li>
+          <li><a href='https://codepen.io/YVenne'>{IconMapper('codepen')}</a></li>
+        </ul>
+      </div>
       <div className={styles.credits}>
         <Button as='link' className={styles.link} variant='secondary' href='https://github.com/YorickVdVenne/Nextjs-Drupal-Portfolio' target='_blank'>
             Built by Yorick Van de Venne
