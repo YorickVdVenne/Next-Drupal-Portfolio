88d2d15e884c5865cf5dbd9abe76bbd0e968f9c6
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2023-10-14 09:51:08 +0200

    fix: header section responsiveness

--- frontend/src/components/organisms/Header/Component.tsx
+++ frontend/src/components/organisms/Header/Component.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
 import { Button } from '@components/atoms/Button/Component'
-import gridStyles from '@components/atoms/Grid/styles.module.css'
-import clsx from 'clsx'
 
 export default function Header (): JSX.Element {
 
@@ -11,7 +9,7 @@ export default function Header (): JSX.Element {
       <h1 className={styles.h1}>Hi, my name is</h1>
       <h2 className={styles.h2}>Yorick Van de Venne.</h2>
       <h3>I build things for the web.</h3>
-      <div className={clsx(gridStyles.grid, styles.wrapper)}>
+      <div className={styles.wrapper}>
         <p className={styles.text}>
           Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more.
           Some nice text about me, even more. Some nice text about me, even more. Some nice text about me, even more. <Button as='link' onClick={() => console.log('test')}>Link</Button>
